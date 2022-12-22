86f4e0b7e7967c79e8c1be417c43259bb9024483
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-12-22 10:10:25 -0300

    added bottomtabnavigation

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -23,6 +23,7 @@ export function Signin() {
             <VStack flex={1}  px={10}>
                 <Image
                     source={BackgroundImg}
+                    defaultSource={BackgroundImg}
                     alt="Pessoas malhando"
                     resizeMode="contain"
                     position='absolute'
