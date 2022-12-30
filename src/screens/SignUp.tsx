5b51cc117d0e9fcf58eddd0141aa5f6d1f6d500c
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-12-30 09:44:48 -0300

    validation work and finished

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,66 +1,150 @@
-import {ScrollView} from 'react-native'
+import { ScrollView } from "react-native";
 import { VStack, Image, Text, Center, Heading } from "native-base";
 
-import LogoSvg from '@assets/logo.svg'
-import BackgroundImg from '@assets/background.png'
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import { useForm, Controller } from "react-hook-form";
+
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
+
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe o nome"),
+  email: yup.string().required("Informe o e-mail").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Inform a senha")
+    .min(6, "A senha deve ter pelo menos 6 dígitos"),
+  passwordConfirm: yup
+    .string()
+    .required("Confirme sua senha")
+    .oneOf([yup.ref("password"), null], "As senhas não são iguais"),
+});
 
 export function SignUp() {
-    const { goBack } = useNavigation()
+  const { goBack } = useNavigation();
 
-    function handleGoBack() {
-        goBack()
-    }
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  });
 
-    return (
-        <ScrollView
-            contentContainerStyle={{flexGrow: 1}} 
-            showsVerticalScrollIndicator={false}
-        >
-            <VStack flex={1}  px={10}>
-                <Image
-                    source={BackgroundImg}
-                    defaultSource={BackgroundImg}
-                    alt="Pessoas malhando"
-                    resizeMode="contain"
-                    position='absolute'
-                />
+  function handleGoBack() {
+    goBack();
+  }
 
-                <Center  my={24}>
-                
+  function handleSignUp(data: FormDataProps) {}
 
-                <LogoSvg />
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas malhando"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-                <Text color="gray.100" fontSize='sm'>Treine sua mente e seu corpo</Text>
-                </Center>
-                
-                <Center>
-                    <Heading color="gray.100" fontSize='xl' mb={6} fontFamily="heading">
-                        Crie sua conta
-                    </Heading>
+        <Center my={24}>
+          <LogoSvg />
 
-                    <Input
-                        placeholder="Nome"
-                    />
-                    <Input 
-                        placeholder="E-mail"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                    />
-                    <Input
-                        placeholder="Senha"
-                        secureTextEntry 
-                    />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
 
-                    <Button title="Criar e acessar" />
-                </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
 
-                <Button mt={24} title="Voltar para o login" variant="outline" onPress={handleGoBack} />
-                
-            </VStack>
-        </ScrollView>
-        
-    )
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="passwordConfirm"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.passwordConfirm?.message}
+              />
+            )}
+          />
+
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
+        </Center>
+
+        <Button
+          mt={12}
+          title="Voltar para o login"
+          variant="outline"
+          onPress={handleGoBack}
+        />
+      </VStack>
+    </ScrollView>
+  );
 }
